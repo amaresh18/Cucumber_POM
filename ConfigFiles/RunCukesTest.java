cf21e18739794f7f5e70a8fb7b8361cf5687fcd4
GajawAma <Amaresh.Gajawada@adp.com>
2018-05-10 01:50:52 +0530

    Changed the packages for MyStepdefs.java and RunCukesTest

---
+++ ConfigFiles/RunCukesTest.java
@@ -0,0 +1,29 @@
+package com.cucumber.runner;
+
+import java.io.File;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+import com.cucumber.listener.Reporter;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * A sample test to demonstrate
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/features"},
+		glue = {"com.pages.pageobjects"},
+		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:"}
+		)
+public class RunCukesTest {
+
+	@AfterClass
+	public static void teardown() {
+		Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+		Reporter.setSystemInfo("user", System.getProperty("user.name"));
+		Reporter.setSystemInfo("os", "Windows 10");
+		Reporter.setTestRunnerOutput("Sample test runner output message");
+	}
+
+}
